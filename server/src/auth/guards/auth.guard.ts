2d91bd9ccaf62fe0482dd1f4f757b4ba7c14596c
astrokachi <astrokachi@gmail.com>
2025-04-20 23:38:15 +0100

    chore: add access token verification to guard

--- server/src/auth/guards/auth.guard.ts
+++ server/src/auth/guards/auth.guard.ts
@@ -5,11 +5,25 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
+import { GoogleAuthService } from '../config/google.auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): Promise<boolean> | boolean {
+  constructor(private googleAuthService: GoogleAuthService) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: FastifyRequest = context.switchToHttp().getRequest();
+
+    const accessToken = request.headers['authorization']?.split(' ')[1];
+    if (!accessToken) {
+      throw new UnauthorizedException('Access token not found');
+    }
+
+    const isValid = await this.googleAuthService.verifyAccessToken(accessToken);
+    if (!isValid) {
+      throw new UnauthorizedException('Invalid access token');
+    }
+
     if (request.session && request.session.user) {
       return true;
     }
